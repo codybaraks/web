338758bcd1f12f4f9f3b4f750124f4ac75999d7d
codybaraks <earvinbaraka@gmail.com>
2018-11-19 14:23:25 +0300

    sql

--- app.py
+++ app.py
@@ -5,8 +5,26 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template("index.html")
+   return render_template("index.html")
+
 
 
 if __name__ == '__main__':
     app.run()
+
+
+#count females
+   # SELECT COUNT(*) FROM students WHERE gender='female'
+#select females
+   # SELECT * FROM `students` WHERE gender='female'
+
+#count people born after 1998
+   # SELECT COUNT(*) FROM students WHERE dob >= 1998
+#count people whose names start with letter M
+   # SELECT COUNT(*) FROM students WHERE names<='M%'
+
+#count unique course
+   # SELECT COUNT(DISTINCT course) FROM students
+
+#delate person whose id = 1000
+   # DELETE FROM `students` WHERE id=1000
